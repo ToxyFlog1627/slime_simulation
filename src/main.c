263bc24b7ff3454e4d1002b3264350761c3e9fc1
ToxyFlog1627 <toxymail1627@gmail.com>
2023-04-11 17:10:22 +0300

    Moving slime

--- src/main.c
+++ src/main.c
@@ -1,28 +1,57 @@
-#define MEMORY 131072;
+#define PI 3.14159265359
+#define TWO_PI 6.28318530718
+
+#define likely(x)  __builtin_expect((x), 1)
+#define unlikely(x)  __builtin_expect((x), 0)
+#define abs(a) (a < 0 ? -a : a)
+#define min(a, b) (a < b ? a : b)
+#define max(a, b) (a > b ? a : b)
+#define red(c) (c & 0x000000ff)
+#define green(c) ((c & 0x0000ff00) >> 8)
+#define blue(c) ((c & 0x00ff0000) >> 16)
+#define color(r, g, b) (0xff000000 | (b << 16) | (g << 8) | r)
+#define interpolate(si, ei, sv, ev, xv) ((xv - sv) * (ei - si) / (ev - sv) + si)
+#define blend_colors(c1, c2, k) color(round(interpolate(red(c1), red(c2), 0, 1, k)), round(interpolate(green(c1), green(c2), 0, 1, k)), round(interpolate(blue(c1), blue(c2), 0, 1, k)))
+
+#define forEachSlime() for (Slime *slime = slimes;slime < memoryEnd;slime++)
+
+extern float debug(float degree);
+extern float sin(float degree);
+extern float cos(float degree);
+extern float random(void);
+extern int round(float value);
 
 typedef unsigned int uint;
-typedef struct Slime {
-    int x, y;
-    int dir;
-} Slime;
+typedef struct Slime { float x, y, dir; } Slime;
 
-static uint *pixelMemory, *slimeMemory;
-static int width, height, slimes;
+static uint bg, fg, *pixels;
+static Slime *slimes, *memoryEnd;
+static int width, height, slimesNumber, fadeRate;
 
 int getSlimeSize() { return sizeof(Slime); }
 
-inline uint color(int r, int g, int b) { return 0xff000000 | ((b % 256) << 16) | ((g % 256) << 8) | (r % 256); }
+void init(uint* memoryStart, int _width, int _height, int _slimesNumber, int _fadeRate, uint _bg, uint _fg) {
+    width = _width; height = _height; slimesNumber = _slimesNumber; fadeRate = _fadeRate; bg = _bg; fg = _fg;
 
-void init(int _width, int _height, int _slimes) {
-    width = _width; height = _height; slimes = _slimes;
-    pixelMemory = (uint*) MEMORY;
-    slimeMemory = pixelMemory + width * height * 4;
+    pixels = memoryStart;
+    slimes = pixels + width * height;
+    memoryEnd = slimes + slimesNumber;
+
+    int cx = width / 2, cy = height / 2;
+    forEachSlime() *slime = (Slime) {cx, cy, random() * TWO_PI};
+    for (int i = 0;i < width * height;i++) pixels[i] = bg;
 }
 
 void update() {
-    for (int y = 0;y < height;y++) {
-        for (int x = 0;x < width;x++) {
-            pixelMemory[y * width + x] = color(x / (float) width * 255, y / (float) height * 255, 0);
+    for (int i = 0;i < width * height;i++) pixels[i] = color(max(red(bg), red(pixels[i]) - fadeRate), max(green(bg), green(pixels[i]) - fadeRate), max(blue(bg), blue(pixels[i]) - fadeRate));
+    forEachSlime() {
+        pixels[round(slime->y) * width + round(slime->x)] = fg;
+        float nx = slime->x + cos(slime->dir);
+        float ny = slime->y - sin(slime->dir);
+        if (unlikely(nx < 0 || nx > width || ny < 0 || ny > height)) {
+            // TODO: bounce off
         }
+        slime->x = nx;
+        slime->y = ny;
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-const interval = 100;
-const slimes = 1000;
+const interval = 5;
+const slimes = 1;
+const fadeRate = 1;
+
 const width = window.innerWidth;
 const height = window.innerHeight;
 
@@ -10,16 +12,17 @@ const ctxt = canvas.getContext("2d");
 
 const render = w => {
 	const exports = w.instance.exports;
-
 	exports.memory.grow(Math.ceil((width * height * 4 + slimes * exports.getSlimeSize()) / 65535));
 
 	const {init, update, memory} = exports;
-	init(width, height, slimes);
+	const memoryStart = exports.__heap_end.value;
 
-	// setInterval(() => {
-	update();
-	ctxt.putImageData(new ImageData(new Uint8ClampedArray(memory.buffer, exports.__heap_end.value, width * height * 4), width), 0, 0);
-	// }, interval);
+	init(memoryStart, width, height, slimes, fadeRate, 0xff181818, 0xffffffff);
+
+	setInterval(() => {
+		update();
+		ctxt.putImageData(new ImageData(new Uint8ClampedArray(memory.buffer, memoryStart, width * height * 4), width), 0, 0);
+	}, interval);
 };
 
-WebAssembly.instantiateStreaming(fetch("main.wasm"), {env: {debug: console.log}}).then(render);
+WebAssembly.instantiateStreaming(fetch("main.wasm"), {env: {random: Math.random, cos: Math.cos, sin: Math.sin, round: Math.round}}).then(render);
